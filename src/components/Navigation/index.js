4a1e6f0740b24382e17b74d345b21ba09dc81df4
Marc Bakker <info@marcbakker.com>
2019-01-14 23:54:50 +0100

    redirect to signIn page when not logged in

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,99 +1,117 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import { compose } from "recompose";
 import { withAuthentication } from "../Session/index.js";
 import * as ROUTES from "../../constants/routes.js";
 import SignOut from "../SignOut/index.js";
 
 import "./index.scss";
 
-const Navigation = ({ authenticatedUser, lastSignInTime }) => {
-	return (
-		<div>
+class Navigation extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+		!this.props.authenticatedUser &&
+			this.props.history.push(ROUTES.SIGN_IN);
+	}
+
+	render() {
+		return (
 			<div>
-				<div className='mainMenu'>
-					<ul>
-						<li id='factuur' className='navMenuItemSelected'>
-							<Link to={ROUTES.INVOICES}>invoices</Link>
-							<ul>
-								<li>
-									<a href='#' className='navMenuItemSelected'>
-										list invoices
-									</a>
-								</li>
-								<li>
-									<a href='#'>new invoice</a>
-								</li>
-							</ul>
-						</li>
-						<li id='nota'>
-							<Link to={ROUTES.BILLS}>bills</Link>
-							<ul>
-								<li>
-									<a href='#'>list bills</a>
-								</li>
-								<li>
-									<a href='#'>new bill</a>
-								</li>
-							</ul>
-						</li>
-						<li id='bedrijf'>
-							<Link to={ROUTES.COMPANIES}>companies</Link>
-							<ul>
-								<li>
-									<a href='#'>list companies</a>
-								</li>
-								<li>
-									<a href='#'>new company</a>
-								</li>
-							</ul>
-						</li>
-						<li id='userSettings'>
-							<Link to={ROUTES.ADMIN}>my data</Link>
-							<ul>
-								<li>
-									<a href='#'>edit</a>
-								</li>
-							</ul>
-						</li>
-						<li id='stats'>
-							<Link to={ROUTES.STATS}>overview</Link>
-							<ul>
-								<li>
-									<a href='#'>Income and expenses</a>
-								</li>
-								<li>
-									<a href='#'>revenue</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-					{authenticatedUser && <SignOut />}
-				</div>
-			</div>
-			<div id='topContentBar'>
-				<div id='welcomeMessage'>
-					{authenticatedUser && `Last login: ${lastSignInTime}`}
-				</div>
 				<div>
-					{authenticatedUser
-						? "logged in. last signed in @ lastSignInTime"
-						: "not logged in"}
+					<div className='mainMenu'>
+						<ul>
+							<li id='factuur' className='navMenuItemSelected'>
+								<Link to={ROUTES.INVOICES}>invoices</Link>
+								<ul>
+									<li>
+										<a
+											href='#'
+											className='navMenuItemSelected'>
+											list invoices
+										</a>
+									</li>
+									<li>
+										<a href='#'>new invoice</a>
+									</li>
+								</ul>
+							</li>
+							<li id='nota'>
+								<Link to={ROUTES.BILLS}>bills</Link>
+								<ul>
+									<li>
+										<a href='#'>list bills</a>
+									</li>
+									<li>
+										<a href='#'>new bill</a>
+									</li>
+								</ul>
+							</li>
+							<li id='bedrijf'>
+								<Link to={ROUTES.COMPANIES}>companies</Link>
+								<ul>
+									<li>
+										<a href='#'>list companies</a>
+									</li>
+									<li>
+										<a href='#'>new company</a>
+									</li>
+								</ul>
+							</li>
+							<li id='userSettings'>
+								<Link to={ROUTES.ADMIN}>my data</Link>
+								<ul>
+									<li>
+										<a href='#'>edit</a>
+									</li>
+								</ul>
+							</li>
+							<li id='stats'>
+								<Link to={ROUTES.STATS}>overview</Link>
+								<ul>
+									<li>
+										<a href='#'>Income and expenses</a>
+									</li>
+									<li>
+										<a href='#'>revenue</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+						{this.props.authenticatedUser && <SignOut />}
+					</div>
 				</div>
+				<div id='topContentBar'>
+					<div id='welcomeMessage'>
+						{this.props.authenticatedUser &&
+							`Last login: ${this.props.lastSignInTime}`}
+					</div>
+					<div>
+						{this.props.authenticatedUser
+							? `Last signed in @ ${this.props.lastSignInTime}`
+							: "not logged in"}
+					</div>
 
-				<div id='languageSwitch'>
-					<a
-						href='/index.cfm?event=factuur.list&amp;dir=desc&amp;order=id&amp;page=1&amp;ISOEndDate=&amp;ISOStartDate=&amp;language=en'
-						className='active'>
-						engels
-					</a>
-					&nbsp;/
-					<a href='/index.cfm?event=factuur.list&amp;dir=desc&amp;order=id&amp;page=1&amp;ISOEndDate=&amp;ISOStartDate=&amp;language=nl'>
-						nederlands
-					</a>
+					<div id='languageSwitch'>
+						<a
+							href='/index.cfm?event=factuur.list&amp;dir=desc&amp;order=id&amp;page=1&amp;ISOEndDate=&amp;ISOStartDate=&amp;language=en'
+							className='active'>
+							engels
+						</a>
+						&nbsp;/
+						<a href='/index.cfm?event=factuur.list&amp;dir=desc&amp;order=id&amp;page=1&amp;ISOEndDate=&amp;ISOStartDate=&amp;language=nl'>
+							nederlands
+						</a>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
-export default withAuthentication(Navigation);
+export default compose(
+	withAuthentication,
+	withRouter
+)(Navigation);
