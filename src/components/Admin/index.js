58edee5e004b10f0708b459251ebaf9e4b6474da
Marc Bakker <marc.bakker@rabobank.nl>
2020-02-08 18:58:41 +0100

    added basis for css grid

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -3,7 +3,14 @@ import "./index.scss";
 
 class Admin extends Component {
 	render() {
-		return <div>Admin</div>;
+		return (
+			<div className='containerAdmin'>
+				<div className='address'>address</div>
+				<div className='registrations'>registrations</div>
+				<div className='deliveryconditions'>deliveryconditions</div>
+				<div className='footer'>footer</div>
+			</div>
+		);
 	}
 }
 
