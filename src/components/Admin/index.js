b90ea559dda4dc67bda73455e63975adee0dcc2a
Marc Bakker <marc.bakker@rabobank.nl>
2020-02-09 19:17:05 +0100

    import css as css module

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import "./index.scss";
+import * as index from "./index.module.scss";
 
 class Admin extends Component {
 	render() {
 		return (
-			<div className='containerAdmin'>
-				<div className='address'>address</div>
-				<div className='registrations'>registrations</div>
-				<div className='deliveryconditions'>deliveryconditions</div>
-				<div className='footer'>footer</div>
+			<div className={index.containerAdmin}>
+				<div className={index.address}>address</div>
+				<div className={index.registrations}>registrations</div>
+				<div className={index.deliveryconditions}>deliveryconditions</div>
+				<div className={index.footer}>footer</div>
 			</div>
 		);
 	}
