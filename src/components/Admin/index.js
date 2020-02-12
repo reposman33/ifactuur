8c121a7ead385cad9e4ea3fe11ce8d456048ec5a
Marc Bakker <marc.bakker@rabobank.nl>
2020-02-12 15:40:49 +0100

    add name fields in Admin component

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { InputGroup, FormControl } from "react-bootstrap";
+import "./../App/index.scss";
+import "./index.scss";
 
 class Admin extends Component {
 	render() {
@@ -10,9 +13,30 @@ class Admin extends Component {
 							<legend>
 								<h1>Address</h1>
 							</legend>
-							<div>
-								<label htmlFor=''>Naam</label>
-								<input type='text' />
+
+							<div className='row'>
+								<div className='inputSmall mb-3 ml-3'>
+									<InputGroup size='sm'>
+										<FormControl placeholder='Initials' aria-label='Initials'></FormControl>
+									</InputGroup>
+								</div>
+								<div className='inputXSmall mx-3'>
+									<InputGroup size='sm'>
+										<FormControl placeholder='Pre' aria-label='Pre'></FormControl>
+									</InputGroup>
+								</div>
+								<div className='input mb-3 mr-3 surname'>
+									<InputGroup size='sm'>
+										<FormControl placeholder='Surname' aria-label='Surname'></FormControl>
+									</InputGroup>
+								</div>
+							</div>
+							<div className='row'>
+								<div className='input mb-3 mx-3'>
+									<InputGroup size='sm'>
+										<FormControl placeholder='Address' aria-label='Address'></FormControl>
+									</InputGroup>
+								</div>
 							</div>
 							<div>
 								<label htmlFor=''>Naam</label>
