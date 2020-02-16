4cb0ef86f8791d242df956baa5e2588262fbf9bd
Marc Bakker <marc.bakker@rabobank.nl>
2020-02-16 21:24:52 +0100

    import from: no need to refer to index.js since it is implied.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App/index.js";
+import App from "./components/App";
 import Firebase, { FirebaseContext } from "./components/Firebase/index.js";
 import "./index.scss";
 
