63bcb1c3d9ac81ebd1ee09e9ba18011dfeddaf98
Marc Bakker <marc.bakker@rabobank.nl>
2020-02-16 21:27:14 +0100

    add comment

--- src/services/API/API.ts
+++ src/services/API/API.ts
@@ -1,4 +1,5 @@
 export default class Api {
+	// TODO: implement a real backend instead of local storage...
 	public static savePage(data: { [page: string]: any }) {
 		const _page: string = Object.keys(data)[0];
 		const _data: any = JSON.stringify(data[_page]);
