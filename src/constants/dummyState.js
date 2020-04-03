e952d66836a7a44293dacb64b6cefa4b9068a6f0
Marc Bakker <webdev@marcbakker.com>
2020-04-03 10:05:00 +0200

    Add dummy state for invoices

--- src/constants/dummyState.js
+++ src/constants/dummyState.js
@@ -41,6 +41,181 @@ export const state = {
 			company: "Akzo",
 			sum: "9.855, -",
 			status: "open"
+		},
+		{
+			id: "106",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "107",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "108",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "109",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "110",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "111",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "112",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "113",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "114",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "115",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "116",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "117",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "118",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "119",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "120",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "121",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "122",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "123",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "124",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "125",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "126",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "127",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "128",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "129",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
+		},
+		{
+			id: "130",
+			date: "17 februari 2020",
+			company: "Akzo",
+			sum: "9.855, -",
+			status: "open"
 		}
 	]
 };
