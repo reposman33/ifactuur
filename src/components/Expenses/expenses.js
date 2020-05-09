8b020a614bac02b2fb5b7efd956465e1c3763fdb
Marc Bakker <webdev@marcbakker.com>
2020-05-09 21:40:24 +0200

    remove console.log

--- src/components/Expenses/expenses.js
+++ src/components/Expenses/expenses.js
@@ -31,7 +31,6 @@ class Expenses extends React.Component {
 	componentDidMount() {
 		this.expensesPromise.then((data) => {
 			this.setState({ rowData: data });
-			console.log("expenses = ", data);
 		});
 	}
 
