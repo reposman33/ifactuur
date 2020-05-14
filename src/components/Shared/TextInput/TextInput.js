b80e81e83bba156f80bbc49a481f29d5da78276f
Marc Bakker <webdev@marcbakker.com>
2020-05-14 19:02:27 +0200

    Component: add props for extraClasses and extraStyles.

--- src/components/Shared/TextInput/TextInput.js
+++ src/components/Shared/TextInput/TextInput.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import styles from "./textInput.module.scss";
 
-const TextInput = ({ displayInput, displayValue, labelText, type, name, handleOnChange }) => {
+const TextInput = ({
+	extraClasses,
+	extraStyles,
+	displayInput,
+	displayValue,
+	labelText,
+	type,
+	name,
+	handleOnChange,
+}) => {
 	const [value, setValue] = useState("");
+	const _extraClasses = extraClasses ? " " + extraClasses : "";
 	const onChange = (event) => {
 		setValue(event.target.value);
 		handleOnChange(event.target.name, event.target.value);
 	};
 
 	return (
-		<div className={styles.container + " d-flex flex-column"}>
+		<div className={styles.container + " d-flex flex-column" + _extraClasses}>
 			<label>{labelText}</label>
 			{displayInput ? (
 				<input
@@ -19,6 +29,7 @@ const TextInput = ({ displayInput, displayValue, labelText, type, name, handleOn
 					step='any'
 					type={type}
 					value={value}
+					style={extraStyles}
 				/>
 			) : (
 				<span>{displayValue}</span>
