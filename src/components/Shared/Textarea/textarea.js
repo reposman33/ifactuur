c1567b863e6fc654cccd2f2bd89326cd36dd5cc5
Marc Bakker <webdev@marcbakker.com>
2020-05-19 20:14:17 +0200

    CSS:  remove container class from top-level element and option to remove it.

--- src/components/Shared/Textarea/textarea.js
+++ src/components/Shared/Textarea/textarea.js
@@ -4,7 +4,6 @@ import styles from "./textarea.module.scss";
 /**
  *
  * @param {number}		cols - number of columns of textrea element
- * @param {boolean}		container - true - display or don't display container class on top level element
  * @param {array}		data - array holding key-value data for all the select options
  * @param {boolean}		displayInput - true: display user input DOM element; false: display existing value instead
  * @param {string}		displayValue - text to display instead of allowing userInput
@@ -15,28 +14,13 @@ import styles from "./textarea.module.scss";
  * @param {number}		rows - number of rows of textrea element
  */
 
-const Textarea = ({
-	cols,
-	container = true,
-	displayInput,
-	displayValue,
-	extraClasses,
-	handleOnChange,
-	labelText,
-	name,
-	rows,
-}) => {
+const Textarea = ({ cols, displayInput, displayValue, extraClasses, handleOnChange, labelText, name, rows }) => {
 	const onChange = (event) => {
 		handleOnChange(event.target.name, event.target.value);
 	};
 
 	return (
-		<div
-			className={
-				(container ? styles.container + " " : "") +
-				"d-flex flex-column" +
-				(extraClasses ? " " + extraClasses : "")
-			}>
+		<div className={"d-flex flex-column" + (extraClasses ? " " + extraClasses : "")}>
 			<label>{labelText}</label>
 			<textarea
 				className={styles.description}
