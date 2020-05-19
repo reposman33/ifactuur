2658198c887f33d853920a5de864a3a66e21f51d
Marc Bakker <webdev@marcbakker.com>
2020-05-19 12:24:55 +0200

    CSS: Remove container class (border) around component; Refactor: remove useState it is not used / necessary.

--- src/components/Shared/Textarea/textarea.js
+++ src/components/Shared/Textarea/textarea.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import styles from "./textarea.module.scss";
 
-const Textarea = ({ labelText, name, displayValue, displayInput, handleOnChange, cols, rows, extraClasses }) => {
-	const [value, setValue] = useState(displayValue);
+const Textarea = ({ cols, displayInput, displayValue, extraClasses, handleOnChange, labelText, name, rows }) => {
 	const _extraClasses = extraClasses ? " " + extraClasses : "";
 	const onChange = (event) => {
-		setValue(event.target.value);
 		handleOnChange(event.target.name, event.target.value);
 	};
 
 	return (
-		<div className={styles.container + " d-flex flex-column mx-3 mb-3" + _extraClasses}>
+		<div className={"d-flex flex-column mx-3 mb-3" + _extraClasses}>
 			<label>{labelText}</label>
 			<textarea
 				className={styles.description}
