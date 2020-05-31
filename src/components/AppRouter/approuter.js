4f419ca94c5f3f15254c650604a9c67daec74dd3
Marc Bakker <webdev@marcbakker.com>
2020-05-31 21:41:41 +0200

    The recommended method of rendering something with a <Route> is to use children elements.

--- src/components/AppRouter/approuter.js
+++ src/components/AppRouter/approuter.js
@@ -17,18 +17,18 @@ import Stats from "../Stats/stats";
 const AppRouter = () => (
 	<Switch>
 		<Redirect exact from='' to={ROUTES.INVOICES} />
-		<Route exact path={ROUTES.COMPANIES} component={Companies} />
-		<Route exact path={ROUTES.COMPANY} component={Company} />
-		<Route exact path={ROUTES.SETTINGS} component={Settings} />
-		<Route exact path={ROUTES.EXPENSES} component={Expenses} />
-		<Route exact path={ROUTES.EXPENSE} component={Expense} />
-		<Route exact path={ROUTES.INVOICES} component={Invoices} />
-		<Route exact path={ROUTES.INVOICE} component={Invoice} />
-		<Route exact path={ROUTES.PASSWORD_CHANGE} component={PasswordChange} />
-		<Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
-		<Route exact path={ROUTES.SIGN_IN} component={SignIn} />
-		<Route exact path={ROUTES.SIGN_UP} component={SignUp} />
-		<Route exact path={ROUTES.STATS} component={Stats} />
+		<Route exact path={ROUTES.COMPANIES} render={(props) => <Companies {...props} />} />
+		<Route exact path={ROUTES.COMPANY} render={(props) => <Company {...props} />} />
+		<Route exact path={ROUTES.SETTINGS} render={(props) => <Settings {...props} />} />
+		<Route exact path={ROUTES.EXPENSES} render={(props) => <Expenses {...props} />} />
+		<Route exact path={ROUTES.EXPENSE} render={(props) => <Expense {...props} />} />
+		<Route exact path={ROUTES.INVOICES} render={(props) => <Invoices {...props} />} />
+		<Route exact path={ROUTES.INVOICE} render={(props) => <Invoice {...props} />} />
+		<Route exact path={ROUTES.PASSWORD_CHANGE} render={(props) => <PasswordChange {...props} />} />
+		<Route exact path={ROUTES.PASSWORD_FORGET} render={(props) => <PasswordForget {...props} />} />
+		<Route exact path={ROUTES.SIGN_IN} render={(props) => <SignIn {...props} />} />
+		<Route exact path={ROUTES.SIGN_UP} render={(props) => <SignUp {...props} />} />
+		<Route exact path={ROUTES.STATS} render={(props) => <Stats {...props} />} />
 	</Switch>
 );
 
