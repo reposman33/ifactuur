98df992afcc3bfade15923b6a793531a1eb69629
Marc Bakker <webdev@marcbakker.com>
2020-06-12 16:31:12 +0200

    Add: have to add environments...or will not run.

--- src/environments.js
+++ src/environments.js
@@ -1,19 +1,19 @@
 export const config_dev = {
 	apiKey: "AIzaSyBT8_0sovEhbeBquwHVvgLqUriv-TttJo0",
-	authDomain: "", // process.env.REACT_APP_PROD_AUTH_DOMAIN,
-	databaseURL: "", //  process.env.REACT_APP_PROD_DATABASE_URL,
-	projectId: "", //  process.env.REACT_APP_PROD_PROJECT_ID,
-	storageBucket: "", // process.env.REACT_APP_PROD_STORAGE_BUCKET,
-	messagingSenderId: "", //  process.env.REACT_APP_PROD_MESSAGING_SENDER_ID
-	documentTitle: "iFactuur-v2" //  process.env.REACT_APP_DEV_DOCUMENT_TITLE
+	authDomain: "ifactuur-v2.firebaseapp.com",
+	databaseURL: "https://ifactuur-v2.firebaseio.com",
+	projectId: "ifactuur-v2",
+	storageBucket: "ifactuur-v2.appspot.com",
+	messagingSenderId: "559304969817",
+	appId: "1:559304969817:web:a37b2f2540b097062e6b48"
 };
 
 export const config_prod = {
 	apiKey: "AIzaSyBT8_0sovEhbeBquwHVvgLqUriv-TttJo0",
-	authDomain: "", // process.env.REACT_APP_PROD_AUTH_DOMAIN,
-	databaseURL: "", //  process.env.REACT_APP_PROD_DATABASE_URL,
-	projectId: "", //  process.env.REACT_APP_PROD_PROJECT_ID,
-	storageBucket: "", // process.env.REACT_APP_PROD_STORAGE_BUCKET,
-	messagingSenderId: "", //  process.env.REACT_APP_PROD_MESSAGING_SENDER_ID
-	documentTitle: "iFactuur-v2" //  process.env.REACT_APP_PROD_DOCUMENT_TITLE
+	authDomain: "ifactuur-v2.firebaseapp.com",
+	databaseURL: "https://ifactuur-v2.firebaseio.com",
+	projectId: "ifactuur-v2",
+	storageBucket: "ifactuur-v2.appspot.com",
+	messagingSenderId: "559304969817",
+	appId: "1:559304969817:web:a37b2f2540b097062e6b48"
 };
