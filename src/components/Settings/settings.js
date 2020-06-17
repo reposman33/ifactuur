ae226d3b8cc056b7e771676db5d751903d57fe7f
Marc Bakker <webdev@marcbakker.com>
2020-06-17 17:21:11 +0200

    Fix: In exisitng document, store state in sessionStorage after returning, theoriginal datawas shown, not from sessionStorage. Move retrieval of storage to Promise.all.

--- src/components/Settings/settings.js
+++ src/components/Settings/settings.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withFirebase } from "../../Firebase";
+import { PersistenceContext } from "../../constants/contexts";
 import { ModalComponent } from "../Shared/Modal/Modal";
 import { I18n } from "../../services/I18n/I18n";
 import * as ROUTES from "../../constants/routes";
@@ -11,10 +12,24 @@ import { LanguagePicker } from "../LanguagePicker/languagePicker";
 import "./settings.module.scss";
 
 class Settings extends React.Component {
+	static contextType = PersistenceContext;
+
 	constructor(props) {
 		super(props);
+
 		this.I18n = new I18n();
+		this.storage = undefined; // to be set in componentDidMount
+
 		this.state = {
+			address: undefined,
+			title: undefined,
+			firstName: undefined,
+			lastName: undefined,
+			city: undefined,
+			zipcode: undefined,
+			country: undefined,
+			companyId: undefined,
+			deliveryConditions: undefined,
 			titles: [
 				{ title: this.I18n.get("TITLES.MR"), id: 1 },
 				{ title: this.I18n.get("TITLES.MRS"), id: 2 },
@@ -22,6 +37,12 @@ class Settings extends React.Component {
 			],
 			companies: [],
 			showModal: false,
+			settingsStatus: {
+				error: false,
+				message: "",
+			},
+			userId: undefined, // the user Id (as defined in user document
+			ID: undefined, // the document id (as defined by fireStore)
 		};
 		// State keys and transform functions to apply when persisting to fireStore .
 		this.persistFields = {
@@ -32,10 +53,9 @@ class Settings extends React.Component {
 			city: (fieldValue) => fieldValue,
 			zipcode: (fieldValue) => fieldValue,
 			country: (fieldValue) => fieldValue,
-			companyId: (fieldValue) => fieldValue,
-			lastlogin: (fieldValue) => fieldValue,
+			companyId: parseInt,
 			deliveryConditions: (fieldValue) => fieldValue,
-			password: (fieldValue) => fieldValue, // hash the password
+			userId: (fieldValue) => fieldValue,
 		};
 		this.isExistingUserSetting = false;
 	}
@@ -45,38 +65,123 @@ class Settings extends React.Component {
 		settingsPromise$.push(this.props.firebase.getUserSettings());
 		settingsPromise$.push(this.props.firebase.getCollection("companies", "name", ["id", "name"]));
 		Promise.all(settingsPromise$).then((values) => {
+			let stateValues;
 			if (values[0]) {
 				this.isExistingUserSetting = true;
 				// populate statekeys (as defined in this.persistFields) with usersettings
-				Object.keys(this.persistFields).map((key) => this.setState({ [key]: values[0][key] }));
-				this.setState({ ID: values[0].ID });
+				stateValues = Object.keys(this.persistFields).reduce((acc, key) => {
+					acc[key] = values[0][key];
+					return acc;
+				}, {});
+				stateValues.ID = values[0].ID;
 			} else {
 				this.isExistingUserSetting = false;
 				// set state keys (as defined in this.persistFields) to undefined
-				Object.keys(this.persistFields).map((key) => this.setState({ [key]: undefined }));
+				stateValues = Object.keys(this.persistFields).reduce((acc, key) => {
+					acc[key] = undefined;
+					return acc;
+				}, {});
 			}
-			this.setState({ companies: values[1], showModal: !this.isExistingUserSetting });
+			this.setState((state) => ({
+				...state,
+				...stateValues,
+				companies: values[1],
+			}));
+			// retrieve the (session)storage class
+			this.storage = this.context;
+			// copy stored state from sessionstorage to state
+			const storedState = this.storage.get("settingsState");
+			if (storedState) {
+				this.setState((state) => ({
+					...state,
+					...storedState,
+				}));
+			}
 		});
 	};
 
-	// onSubmit
-	onSubmit = () => {
-		const settings = {};
-		Object.keys(this.persistFields).map(
-			// filter keys and optionally convert state prop values
-			(key) => {
-				if (this.state[key]) {
-					settings[key] = this.persistFields[key](this.state[key]);
-				}
-				return null;
-			}
-		);
-		// add the current user id!
-		settings.userId = this.props.firebase.auth.currentUser.uid;
-		console.log("submitting ", settings);
-		this.props.firebase.addDocumentToCollection("users", settings, this.state.ID).then((docRef) => {
-			console.log(`document ${this.state.ID ? "updated" : "added"}`);
+	/**
+	 * When user clicks 'New company' store current state and switch to Company component
+	 */
+	handleNewCompany = () => {
+		// copy the state values that will be eventually stored to temporary storage. To be picked up when returning from creating a new company
+		const persistFields = Object.keys(this.persistFields);
+		// store current state under key in (session)Storage
+		const storeValues = persistFields.reduce((acc, persistField) => {
+			acc[persistField] = this.state[persistField];
+			return acc;
+		}, {});
+		storeValues.ID = this.state.ID;
+
+		this.storage.set("settingsState", storeValues);
+		// render Company component
+		this.props.history.push({
+			pathname: ROUTES.COMPANY,
+			params: { prevLocation: this.props.location.pathname, prevLocationName: "ROUTENAME.SETTINGS" },
 		});
+	};
+
+	/**
+	 * submit the invoice
+	 */
+	onSubmit = () => this.storeSettings(this.checkSettings(this.onCreateSettings()));
+
+	/**
+	 * Check is all fields are valid, convert to correct type
+	 * @returns{object} settings - the userSettings, optionally with an error key
+	 */
+	onCreateSettings = () => {
+		return Object.keys(this.persistFields).reduce(
+			(acc, key) => {
+				acc = this.assertFieldOfType[key](this.state[key]) // check if field is valid...
+					? Object.assign(acc, { [key]: this.persistFields[key](this.state[key]) }) // if so, convert string to correct type
+					: Object.assign(acc, { error: true, keys: [...acc.keys, key] }); // else error
+				return acc;
+			},
+			{ eror: false, keys: [] }
+		);
+	};
+
+	/**
+	 * @param{object} settings - the userSettings. Pass if no error, setState if error
+	 * @returns{boolean|object} - false in case of error | userSettings object if no error
+	 * @sideEffect - sets this.state.settingsStatus if settings is not valid
+	 */
+	checkSettings = (settings) => {
+		if (settings.error) {
+			console.log("ERROR: ", settings);
+			this.setState({
+				settingsStatus: {
+					error: true,
+					message:
+						this.I18n.get("USERSETTINGS.SUBMIT.ERROR.MISSINGFIELDVALUES") +
+						" [ " +
+						settings.keys.join(", ") +
+						" ]",
+				},
+			});
+			return false;
+		} else {
+			return settings;
+		}
+	};
+
+	/**
+	 * @param{boolean|object} false|invoice - this param is false if settings is invalid | param is the settings if settings is valid.
+	 * @returns void - calls fireStore as an sideEffect
+	 */
+	storeSettings = (settings) => {
+		if (settings) {
+			// add the default statustitle
+			this.props.firebase
+				.addDocumentToCollection("users", settings, settings.ID)
+				.then((docRef) => {
+					console.log("document ", docRef.id, " added");
+					// remove the temporary state
+					this.storage.remove("settingsState");
+				})
+				.catch((e) => console.log("ERROR: ", e));
+		}
 	};
 
 	/**
@@ -170,7 +275,7 @@ class Settings extends React.Component {
 							)}
 							labelText={this.I18n.get("USERSETTINGS.LABEL.COMPANY")}
 							name='companyId'
-							onButtonClick={() => this.props.history.push({ pathname: ROUTES.COMPANY })}
+							onButtonClick={this.handleNewCompany}
 							valueKey='id'
 						/>
 					</div>
@@ -241,10 +346,12 @@ class Settings extends React.Component {
 						/>
 					</div>
 				</div>
-				<div className='row pt-3'>
-					<div className='d-flex w-100 mt-3 justify-content-end'>
+				<div className='row justify-content-end align-items-baseline pr-2 pt-3'>
+					<div className='text-white mr-3'>
+						{this.state.settingsStatus.error && this.state.settingsStatus.message}
+					</div>
+					<div>
 						<Button
-							extraStyles={{ marginRight: "0.8rem", marginTop: "-2rem" }}
 							onClick={this.onSubmit}
 							text={
 								this.isExistingUserSetting
