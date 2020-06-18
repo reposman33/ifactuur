227dfc0e64b3c193ea5b724437a32117fdd7765d
Marc Bakker <webdev@marcbakker.com>
2020-06-18 19:03:10 +0200

    Add title property to show title on button.

--- src/components/Shared/Button/button.js
+++ src/components/Shared/Button/button.js
@@ -8,15 +8,17 @@ import * as styles from "./button.module.scss";
  * @param {string}		extraStyles - extra styles to apply to parent element
  * @param {function} 	onClick - function prop from parent
  * @param {string}		text - text to display on button
+ * @param {string}		title - title to display on button
  */
 
-const Button = ({ disabled = false, extraClasses, extraStyles, onClick, text }) => {
+const Button = ({ disabled = false, extraClasses, extraStyles, onClick, text, title = "" }) => {
 	return (
 		<button
 			className={(extraClasses ? extraClasses + " " : "") + "d-flex btn btn-primary " + styles.button}
 			style={extraStyles}
 			onClick={onClick}
-			disabled={disabled}>
+			disabled={disabled}
+			title={title}>
 			{text}
 		</button>
 	);
