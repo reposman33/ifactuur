48df208adfff338742758282a56eebe08a90020a
Marc Bakker <webdev@marcbakker.com>
2020-06-18 19:01:16 +0200

    Change onChange() to return the  valueKey value to parent instead of the displayKey value;  Set the displayKey value as the selected option in the select on 1st display;  Fix option to have valueKey as value instead of displayKey

--- src/components/Shared/Select/select.js
+++ src/components/Shared/Select/select.js
@@ -3,18 +3,19 @@ import styles from "./select.module.scss";
 /**
  *
  * @param {string}		buttonText - key holding the value data for the select option
+ * @param {boolean}		columnView - true - display label and select in flex-column view
  * @param {boolean}		container - true - display or don't display container class on top level element
  * @param {array}		data - array holding key-value data for all the select options
- * @param {string}		displayKey - key holding the data to display in the select option
  * @param {boolean}		displayInput - true: display user input DOM element; false: display existing value instead
+ * @param {string}		displayKey - key holding the data to display in the select option
  * @param {string}		displayValue - text to display instead of allowing userInput
  * @param {string}		extraClasses - extra classes to apply to parent element
+ * @param {object}		extraStyles - extra inline styles to apply to parent element
  * @param {function} 	handleOnChange - update parent state with user input
- * @param {number} 		initialSelectedValue - The initially selected select value
  * @param {string}		labelText - text to display as label
  * @param {string}		name - input type name
  * @param {function} 	onButtonClick - execute when user clicks the button
- * @param {string}		valueKey - key holding the value datafor the select option
+ * @param {string}		valueKey - key holding the value data for the select option
  */
 const Select = ({
 	buttonText,
@@ -30,12 +31,22 @@ const Select = ({
 	labelText,
 	name,
 	onButtonClick,
+	valueKey,
 }) => {
 	const onChange = (event) => {
-		handleOnChange(event.target.name, event.target.value);
+		// Retrieve the id of the _textcontent_ of the selected menu option
+		const value = Array.from(event.target.options)
+			.filter((option) => option.selected)
+			.reduce((acc, selectedOption) => selectedOption.textContent, "");
+		handleOnChange(name, value);
 	};
+	// if a buttons needs displayhed, make room for it
 	const selectStyle = buttonText ? { width: "50%" } : { margin: 0 };
 
+	//  display the current state value
+	const initialSelectedValue = data
+		.filter((ob) => ob[displayKey] === displayValue)
+		.reduce((acc, selectedOb) => selectedOb[valueKey], "");
 	return (
 		<div
 			className={
@@ -51,13 +62,11 @@ const Select = ({
 						<select
 							name={name}
 							onChange={onChange}
-							value={displayValue}
+							value={initialSelectedValue}
 							style={Object.assign(selectStyle, extraStyles)}>
-							<option key={""} value={null}>
-								---
-							</option>
+							<option key={""} value={""}></option>
 							{data.map((ob, i) => (
-								<option key={i} value={ob[displayKey]}>
+								<option key={i} value={ob[valueKey]}>
 									{ob[displayKey]}
 								</option>
 							))}
