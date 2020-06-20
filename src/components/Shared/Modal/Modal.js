b222731d351a386e29d3968e3e583bbd9f8c676a
Marc <marc.webdev@yahoo.com>
2020-06-20 19:49:50 +0200

    Fix layout Modal
    Do not center the modal;
    No border between header/body and footer.

--- src/components/Shared/Modal/Modal.js
+++ src/components/Shared/Modal/Modal.js
@@ -4,8 +4,8 @@ import "./Modal.scss";
 
 const ModalComponent = (props) => {
 	return (
-		<Modal centered show={props.showModal} backdrop={true} onHide={props.closeModal}>
-			<Modal.Header closeButton={true}>{props.header}</Modal.Header>
+		<Modal show={props.showModal} backdrop={true} onHide={props.closeModal}>
+			<Modal.Header closeButton={false}>{props.header}</Modal.Header>
 			<Modal.Body>{props.body}</Modal.Body>
 			<Modal.Footer>{props.footer}</Modal.Footer>
 		</Modal>
