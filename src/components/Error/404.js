b416024297b3319e3e465a9f2435413d6e289b59
Marc Bakker <webdev@marcbakker.com>
2020-10-06 20:27:09 +0200

    Added 404 page and netlify.toml with configuration for the custom 404 page

---
+++ src/components/Error/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const _404 = () => {
+	return (
+		<div>
+			<h1>Oops! deze pagina bestaat niet!</h1>
+			Ga <a href='window.history.back()'>terug</a> waar je vandaan kwam.
+		</div>
+	);
+};
+
+export default _404;
