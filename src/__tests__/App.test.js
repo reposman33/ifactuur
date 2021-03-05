7fe923d154c5d13a1850ceb2794b8caf6e5f188a
Marc Bakker <webdev@marcbakker.com>
2021-03-05 14:00:01 +0100

    Some preliminary set ups for Jest testing.

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '../components/App/index';
+import renderer from 'react-test-renderer';
+
+describe('The sign in form', () => {
+    it('...should fail...', () => {
+        expect(false).toBeFalsy()
+    })
+	it('...should sometimes succeed ;)', () => {
+		expect(true).toBeFalsy()
+	})
+});
--- src/test/App.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from '../components/App/index';
-import renderer from 'react-test-renderer';
-
-describe('THe sign in form', () => {
-    it('should render', () => {
-        const  
-    })
-});
